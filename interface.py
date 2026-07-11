9c584e5a68bb9903f388953035800ba06c7fc908
ELEUCHANAS <anaseleuch@etudiant-fst.utm.tn>
2026-07-11 23:11:45 +0300

    Liaison des Label/Entry/boutons avec les fonctions chiffrer/dechiffrer de cipher.py

--- interface.py
+++ interface.py
@@ -1,15 +1,22 @@
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk,messagebox
+from cipher import chiffrer,dechiffrer
+from utils import is_binary,is_Hex,is_base64
 def Init_Encrypt_Frame(win):
+    global Encrypt_box
+    global Encrypt_Plaintext_Entry
+    global Encrypt_Key_Entry
+    global Encrypt_Ciphertext_label
     Encrypt_frame = tk.Frame(win,borderwidth=3)
     Encrypt_title_label = tk.Label(Encrypt_frame,text="Encryption")
     Encrypt_Plaintext_label = tk.Label(Encrypt_frame,text="PlainText : ")
     Encrypt_Key_label = tk.Label(Encrypt_frame,text="Key : ")
     Encrypt_InputType_label = tk.Label(Encrypt_frame,text="Input type : ")
     Encrypt_box = ttk.Combobox(Encrypt_frame,values=["UTF-8","Hexadecimal","Binaire"])
-    Encrypt_Bouton = tk.Button(Encrypt_frame,text="Encrypt")
+    Encrypt_Bouton = tk.Button(Encrypt_frame,text="Encrypt",command=Gui_Encrypt)
     Encrypt_Plaintext_Entry = tk.Entry(Encrypt_frame,width=20)
     Encrypt_Key_Entry = tk.Entry(Encrypt_frame,width=20)
+    Encrypt_Ciphertext_label = tk.Label(Encrypt_frame,text="")
     Encrypt_title_label.grid(row=0, column=0, columnspan=2, pady=10)
     Encrypt_Plaintext_label.grid(row=1, column=0, sticky="e", padx=5, pady=5)
     Encrypt_Plaintext_Entry.grid(row=1, column=1, padx=5, pady=5)
@@ -18,17 +25,23 @@ def Init_Encrypt_Frame(win):
     Encrypt_InputType_label.grid(row=3, column=0, padx=5, pady=5)
     Encrypt_box.grid(row=3, column=1, padx=5, pady=5)
     Encrypt_Bouton.grid(row=4, column=0, columnspan=2, pady=10)
+    Encrypt_Ciphertext_label.grid(row=5, column=0, columnspan=2, pady=10)
     Encrypt_frame.grid(row=0, column=0, padx=5, pady=5)
 def Init_Decrypt_Frame(win):
+    global Decrypt_box
+    global Decrypt_CipherText_Entry
+    global Decrypt_Key_Entry
+    global Decrypt_Plaintext_label
     Decrypt_frame = tk.Frame(win,borderwidth=3)
     Decrypt_title_label = tk.Label(Decrypt_frame,text="Decryption")
     Decrypt_CipherText_label = tk.Label(Decrypt_frame,text="CipherText : ")
     Decrypt_Key_label = tk.Label(Decrypt_frame,text="Key : ")
     Decrypt_InputType_label = tk.Label(Decrypt_frame,text="Input type : ")
     Decrypt_box = ttk.Combobox(Decrypt_frame,values=["Hexadecimal","base64","Binaire"])
-    Decrypt_Bouton = tk.Button(Decrypt_frame,text="Decrypt")
+    Decrypt_Bouton = tk.Button(Decrypt_frame,text="Decrypt",command=Gui_Decrypt)
     Decrypt_CipherText_Entry = tk.Entry(Decrypt_frame,width=20)
     Decrypt_Key_Entry = tk.Entry(Decrypt_frame,width=20)
+    Decrypt_Plaintext_label = tk.Label(Decrypt_frame,text="",borderwidth=5)
     Decrypt_title_label.grid(row=0, column=0, columnspan=2, pady=10)
     Decrypt_CipherText_label.grid(row=1, column=0, sticky="e", padx=5, pady=5)
     Decrypt_CipherText_Entry.grid(row=1, column=1, padx=5, pady=5)
@@ -37,6 +50,7 @@ def Init_Decrypt_Frame(win):
     Decrypt_InputType_label.grid(row=3, column=0, padx=5, pady=5)
     Decrypt_box.grid(row=3, column=1, padx=5, pady=5)
     Decrypt_Bouton.grid(row=4, column=0, columnspan=2, pady=10)
+    Decrypt_Plaintext_label.grid(row=5, column=0, columnspan=2, pady=10)
     Decrypt_frame.grid(row=0, column=2, padx=5, pady=5)
 def Init_sep(win):
     win.columnconfigure(0, weight=1)
@@ -80,6 +94,57 @@ def Init_Attack_Frame(win):
     Attack_KeyFound_label.grid(row=5, column=0, columnspan=2, sticky="w", pady=5)
     Attack_Exectime_label.grid(row=6, column=0, columnspan=2, sticky="w", pady=5)
     Attack_frame.grid(row=2, column=2, padx=5, pady=5)
+def Gui_Encrypt(event=None):
+    choice = Encrypt_box.get()
+    key = Encrypt_Key_Entry.get()
+    msg = Encrypt_Plaintext_Entry.get()
+    if not choice:
+        messagebox.showerror("Erreur","Please select an input type")
+        return
+    if is_binary(key,16) != True:
+        messagebox.showerror("Erreur","Please type a valid 16-bit key")
+        return
+    match (choice):
+        case "UTF-8":
+            cin=0
+        case "Hexadecimal":
+            cin=1
+            if is_Hex(msg) != True:
+                messagebox.showerror("Erreur","Please type a valid Hexadecimal Plaintext")
+                return
+        case "Binaire":
+            cin=2
+            if is_binary(msg) != True:
+                messagebox.showerror("Erreur","Please type a valid Binary Plaintext")
+                return
+    Encrypt_Ciphertext_label.config(text=str(chiffrer(msg,int(key,2),cin)))
+def Gui_Decrypt(event=None):
+    choice = Decrypt_box.get()
+    key = Decrypt_Key_Entry.get()
+    msg = Decrypt_CipherText_Entry.get()
+    if not choice:
+        messagebox.showerror("Erreur","Please select an input type")
+        return
+    if is_binary(key,16) != True:
+        messagebox.showerror("Erreur","Please type a valid 16-bit key")
+        return
+    match (choice):
+        case "Hexadecimal":
+            cin=0
+            if is_Hex(msg) != True:
+                messagebox.showerror("Erreur","Please type a valid Hexadecimal Plaintext")
+                return
+        case "base64":
+            cin=1
+            if is_base64(msg) != True:
+                messagebox.showerror("Erreur","Please type a valid base64 Plaintext")
+                return
+        case "Binaire":
+            cin=2
+            if is_binary(msg) != True:
+                messagebox.showerror("Erreur","Please type a valid Binary Plaintext")
+                return
+    Decrypt_Plaintext_label.config(text=str(dechiffrer(msg,int(key,2),cin)))
 def Init_Gui():
     window = tk.Tk()
     window.title("SPN Cipher")
--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import base64
 def XOR(b1,b2):
     res=0
     for i in range(8):
@@ -9,3 +10,21 @@ def RoL(cle,n):
     return (cle << n) & 0xFFFF  | (cle >> 16 - n)
 def Comparer(b1,b2):
     return sum([b1[i] !=b2[i] for i in range(8)])
+def is_binary(b,n = None):
+    try:
+        int(b, 2)
+    except ValueError:
+        return False
+    if n is not None and len(b) != n:
+        return False
+    return True
+def is_Hex(h):
+    return set(h.upper()).issubset("0 1 2 3 4 5 6 7 8 9 A B C D E F".split(" "))
+def is_base64(b):
+    try:
+        if len(b) % 4 != 0:
+            return False
+        base64.b64decode(b, validate=True)
+        return True
+    except Exception:
+        return False
